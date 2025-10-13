7f3fb3e8fab226f8689f31192c30cfada3710ccf
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2025-10-13 07:47:48 +0530

    day 3

--- day2/conditionalStatement.py
+++ day2/conditionalStatement.py
@@ -1,4 +1,4 @@
-#example 1
+# example 1
 
 # num=11
 # if num%2==0:
@@ -6,7 +6,7 @@
 # else:
 #     print("number is odd")
 
-#example 2
+# example 2
 #
 # if 1:
 #     print("one")
@@ -14,29 +14,83 @@
 #     print("zero")
 
 
-#Example 3 if else in oneline (ternary operator)
+# Example 3 if else in oneline (ternary operator)
 # num=11
 # print("Even number") if num%2==0 else print("odd number")
 
-#Example 4 if else - multiple statement in single line
+# Example 4 if else - multiple statement in single line
 
 # num=20
 # {print("Hi"),print("prakash")} if num>=21 else {print("Bye"), print("prakash")}
 
 
+# num=20
+# if num>=10:
+#     print("hi")
+#     print("janna")
+# else:
+#     print("Bye")
+#     print("janna")
 
-num=20
-if num>=10:
-    print("hi")
-    print("janna")
+# if statement
+# Example-1
+# status_code=200;
+# if status_code == 200:
+#     print("pass")
+
+# Example-2
+# age=25
+# if age > 25:
+#     print("Eligible for create an account")
+
+# Example-3
+# username="Admin"
+# if username=="Admin":
+#     print("Welcome Admin")
+
+# 🟡 2️⃣ if…else Statement
+# Example-1
+login_success = True
+if login_success:
+    print("Login Successful")
 else:
-    print("Bye")
-    print("janna")
+    print("Longin Fail")
 
+# Example-2
+mark = 40
+if mark >= 50:
+    print("Pass")
+else:
+    print("Fail")
 
+# Example-3
 
+browser = "Chrome"
+if browser == "Chrome":
+    print("Launching Chrome Browser...")
+else:
+    print("Launching default browser...")
 
+    # if… elif … else Statement
 
+score = 85
+if score >= 90:
+    print("Grade: A")
+elif score >= 75:
+    print("Grade: B")
+elif score >= 60:
+    print("Grade: C")
+else:
+    print("Grade: F")
 
+    # Example 3
 
-
+    browser = "Firefox"
+    if browser == "Chrome":
+        print("Launching Chrome")
+    elif browser == "Firefox":
+        print("Launching Firefox")
+    elif browser == "Edge":
+        print("Launching Edge")
+    else:
+        print("Browser not supported")
