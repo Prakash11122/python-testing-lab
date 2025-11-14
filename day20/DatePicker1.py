545588f264868c1535cd003279894d21ee431aac
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2025-11-14 17:46:18 +0530

    Date Picker

---
+++ day20/DatePicker1.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+serv_obj = Service("/usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=serv_obj)
+
+driver.get("https://jqueryui.com/datepicker/")
+driver.maximize_window()
+
+driver.switch_to.frame(0)
+
+year = "2026"
+month = "November"
+date = "20"
+
+# Open the date picker
+driver.find_element(By.ID, "datepicker").click()
+
+while True:
+    mon = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-month']").text
+    yr = driver.find_element(By.XPATH, "//span[@class='ui-datepicker-year']").text
+
+    if mon == month and yr == year:
+        break
+    else:
+        driver.find_element(By.XPATH, "//a[@title='Next']").click()
+        time.sleep(3)
+
+# Select date
+driver.find_element(By.XPATH, f"//a[text()='{date}']").click()
